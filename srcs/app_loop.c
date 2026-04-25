bfb41f6232cf6be4d5ccfb63f11f146ec0a7e5b7
leonardecavele <leonard.decavele@gmail.com>
2026-04-25 17:10:35 +0200

    new: end view

--- srcs/app_loop.c
+++ srcs/app_loop.c
@@ -6,7 +6,7 @@
 /*   By: ldecavel <ldecavel@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/25 02:38:23 by ldecavel          #+#    #+#             */
-/*   Updated: 2026/04/25 17:07:16 by ldecavel         ###   ########.fr       */
+/*   Updated: 2026/04/25 17:08:06 by ldecavel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,9 @@ extern void app_loop(void) {
 		},
 		.game_view = {
 			.update = game_update, .render = game_render
+		},
+		.end_view = {
+			.update = end_update, .render = end_render
 		},
 		.user_input = -1
 	};
